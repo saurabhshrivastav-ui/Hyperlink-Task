a6e28ba0949fa4b1d807e7c1ca072de65a0f3396
saurabhshrivastav-ui <saurabhshrivastavsms@gmail.com>
2025-12-23 16:52:30 +0530

    Add Task1 folder and its contents

---
+++ Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>🏠 Home Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: "center", justifyContent: "center" },
+  text: { fontSize: 22, fontWeight: "600" },
+});
